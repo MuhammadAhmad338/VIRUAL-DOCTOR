55039256eafca83de27727feee860521c80e0a31
Muhammad Ahmad <ahmadmuhammad.7700@gmail.com>
2024-05-31 04:29:51 -0700

    latest commits and updated commits

--- lib/Helper/const.dart
+++ lib/Helper/const.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-const APIURL = 'https://wellnow-backend.onrender.com';
+const APIURL = 'https://mybestwellnowbackendapp.onrender.com';
 FirebaseStorage storage = FirebaseStorage.instance;
 const tealAccent = Colors.tealAccent;
 const teal = Colors.teal;
